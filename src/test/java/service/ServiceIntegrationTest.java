27902703b4b0c897693a77ea258ac8c3ad793ed1
Iustin <ficauiustin@gmail.com>
2021-04-07 09:11:25 +0300

    Added integration test

---
+++ src/test/java/service/ServiceIntegrationTest.java
@@ -0,0 +1,65 @@
+package service;
+
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
+import org.junit.jupiter.api.Test;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ServiceIntegrationTest {
+
+    String filenameStudent = "fisiere/test/testStudenti.xml";
+    String filenameTema = "fisiere/test/testTeme.xml";
+    String filenameNota = "fisiere/test/testNote.xml";
+    StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+    TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+    NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+    StudentValidator studentValidator = new StudentValidator();
+    TemaValidator temaValidator = new TemaValidator();
+    NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+    Service testService = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+    @Test
+    public void tc1_addTema(){
+        Tema testTema = new Tema("1EN","A test assignment", 10, 10);
+        testService.addTema(testTema);
+        assertTrue(testService.findTema(("1EN")).equals(testTema));
+    }
+
+
+    @Test
+    public void tc2_addStudent(){
+
+        Student testStudent = new Student("234", "testStudent", 933, "test@test.com");
+        testService.addStudent(testStudent);
+        assertTrue(testService.findStudent("234").equals(testStudent));
+    }
+
+    @Test
+    public void tc3_addGrade(){
+        Nota testNota = new Nota("GR1", "234", "1EN", 10,   LocalDate.now());
+        testService.addNota(testNota, "Good job");
+        assertTrue(testService.findNota("GR1")!= null);
+
+    }
+
+    @Test
+    public void t4_integrationTest(){
+        tc1_addTema();
+        tc2_addStudent();
+        tc3_addGrade();
+        testService.deleteNota("GR1");
+        testService.deleteTema("1EN");
+        testService.deleteStudent("234");
+    }
+
+}
